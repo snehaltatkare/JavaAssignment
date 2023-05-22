02744d90165cb547a6cd288d38daf7fb46f50b21
snehaltatkare <132807711+snehaltatkare@users.noreply.github.com>
2023-05-22 17:28:23 +0530

    First Java Assignment

---
+++ assignment1.java
@@ -0,0 +1,12 @@
+public class assignment1 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        System.out.println("Equal:" + (num1 == num2));
+        System.out.println("Not Equal:" + ( num1 != num2));
+        System.out.println("num1 is Greater Than num2 : " + (num1 > num2));
+        System.out.println("num1 is Less Than num2 : " + (num1 < num2));
+
+    }
+}
---
+++ assignment2.java
@@ -0,0 +1,15 @@
+public class assignment2 {
+        public static void main(String[] args) {
+            int num1 = 10;
+            int num = num1 + 2;
+            System.out.println("After addition: num = " + num);
+            num = num - 4;
+            System.out.println("After subtraction: num = " + num);
+            num = num * 7;
+            System.out.println("After multiplication: num = " + num);
+            num = num / 4;
+            System.out.println("After division: num = " + num);
+            num = num % 12;
+            System.out.println("After modulus: num = " + num);
+    }
+}
